8751ba61b1f7c0cf22e7f6025b4ba3f99a75370f
Pedro Ferreira <plferreira4@gmail.com>
2013-02-27 00:35:56 +0000

    Have a way to update the placeholder
    For example if you change directly the text of a div (changing the
    .innerText), you would want to update the placeholder

--- src/jQuery.divPlaceholder.js
+++ src/jQuery.divPlaceholder.js
@@ -1,10 +1,17 @@
 (function ($) {
 	$(document).on('change keydown keypress input', 'div[data-placeholder]', function() {
-		if (this.textContent) {
-			this.setAttribute('data-div-placeholder-content', 'true');
-		}
-		else {
-			this.removeAttribute('data-div-placeholder-content');
-		}
+    updatePlaceholder(this);
 	});
 })(jQuery);
+
+
+function updatePlaceholder(divElement) {
+
+    if (divElement.textContent) {
+        divElement.setAttribute('data-div-placeholder-content', 'true');
+    }
+    
+    else {
+        divElement.removeAttribute('data-div-placeholder-content');
+    }
+}
